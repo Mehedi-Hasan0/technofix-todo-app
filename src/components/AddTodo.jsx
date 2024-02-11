9f69f5f358b3c905fa4e6cf9f115565479587221
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-02-11 12:43:59 +0600

    [UPDATED] Addtodo made dynamic and context provider bug fixed

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
+import { useToDos } from "../context/TodoContext";
 import Button from "./ui/Button";
 
 const AddTodo = () => {
+  // CONTEXT variables
+  const { handleAddToDo } = useToDos();
+  // STATE
+  const [toDo, setToDo] = useState({
+    task: "",
+    priority: "", // using default value low
+  });
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // saving todos to localstorage and to the context
+    handleAddToDo(toDo);
+
+    // making todo empty for new todo
+    setToDo({ task: "", priority: "" });
+  };
+
   return (
-    <form className="sub-container flex flex-col items-center gap-5">
+    <form
+      onSubmit={handleFormSubmit}
+      className="sub-container flex flex-col items-center gap-5"
+    >
       <textarea
         cols="30"
         rows="8"
         maxLength="200"
         placeholder="Write your task here..."
+        value={toDo.task}
+        onChange={(e) => {
+          setToDo((prev) => ({
+            ...prev,
+            task: e.target.value,
+          }));
+        }}
         className="border border-[#ccc] rounded w-full py-2 px-3 placeholder:text-sm placeholder:2xl:text-base placeholder:text-[#ccc]"
       />
       <div className="flex items-center gap-5">
@@ -22,19 +52,42 @@ const AddTodo = () => {
             role="button"
             className="px-6 2xl:px-8 py-1 hover:bg-gray-200 rounded border border-[#ccc] hover:border-transparent transition duration-200 ease-in text-[#5b5b5b] hover:text-[#3e3e3e]"
           >
-            Priority
+            {toDo?.priority && toDo?.priority.length !== 0
+              ? toDo?.priority
+              : "Priority"}
           </div>
           <ul
             tabIndex={0}
             className="dropdown-content z-10 menu p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
+            <li
+              onClick={() => {
+                setToDo((prev) => ({
+                  ...prev,
+                  priority: "Low",
+                }));
+              }}
+            >
               <a>Low</a>
             </li>
-            <li>
+            <li
+              onClick={() => {
+                setToDo((prev) => ({
+                  ...prev,
+                  priority: "Medium",
+                }));
+              }}
+            >
               <a>Medium</a>
             </li>
-            <li>
+            <li
+              onClick={() => {
+                setToDo((prev) => ({
+                  ...prev,
+                  priority: "High",
+                }));
+              }}
+            >
               <a>High</a>
             </li>
           </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import TodoContext from "./context/TodoContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <TodoContext>
+        <App />
+      </TodoContext>
     </BrowserRouter>
   </React.StrictMode>
 );
