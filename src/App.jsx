09c7a7872198dc827859c6ea27b87e28aebee3c2
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-02-11 08:27:57 +0600

    [DONE] Added navbar ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <main>
-      <h2 className="text-5xl text-red-400">Technofix todo app</h2>
+    <main className="main-container flex flex-col justify-center items-center gap-8 overflow-hidden">
+      <h2 className="text-2xl md:text-3xl 2xl:text-4xl font-semibold">
+        TechnoFix todo app
+      </h2>
+      <Navbar />
     </main>
   );
 }
